e7bde83d5c832c5c9e5c12838679c601af32c0f4
lj-1110 <chenliang@chenliangdeMac-mini.local>
2017-04-27 16:04:34 +0800

    test1

---
+++ LJHelp/LJHelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJHelp
+//
+//  Created by 陈亮 on 2017/4/27.
+//  Copyright © 2017年 陈亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LJHelp/LJHelp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJHelp
+//
+//  Created by 陈亮 on 2017/4/27.
+//  Copyright © 2017年 陈亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
